c9d4ec7a4526577e881cdab0d6a41a9a3e216325
efrat <efratk464@gmail.com>
2022-04-24 20:42:25 +0300

    timer after fix

--- timer.py
+++ timer.py
@@ -1,16 +1,19 @@
 import time
-"""
-A function that checks the duration of functions
-"""
+
 def timer(f, *param):
-    start = time.time()
+    """
+    A function that checks the duration of functions
+    :param fanction
+    :param parameter to the fanction
+    :return The time it took to activate the function
+    """
+    Initial_time_check = time.time()
     f(param)
-    time.sleep(1)
-    end = time.time()
-    return(end - start-1)
+    Final_time_check = time.time()
+    return(Final_time_check - Initial_time_check)
 
 
-if __name__=="__main__":
+if __name__ == "__main__":
     print(timer(print, "Hello"))
     print(timer(zip, [1, 2, 3], [4, 5, 6]))
     print()
