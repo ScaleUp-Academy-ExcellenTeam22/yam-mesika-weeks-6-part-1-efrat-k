28728814072f24ec2bfd5eed835043397ed97bea
efrat <efratk464@gmail.com>
2022-04-24 20:49:50 +0300

    name.py after fix

--- name.py
+++ name.py
@@ -1,16 +1,17 @@
-"""
-:param List of first and last names and optional number
-:returns integration of all names that can be
-"""
-def full_names(first_name, lest_name,min_length=0):
-    if min_length ==0:
-        names=[(first,last) for first in first_name for last in lest_name]
+
+def full_names(first_name, last_name, min_length=0):
+    """
+    :param List of first and last names and optional number
+    :returns integration of all names that can be
+    """
+    if min_length == 0:
+        names = [(first,last) for first in first_name for last in last_name]
     else:
-        names = [(first, last) for first in first_name for last in lest_name if len(first)+len(last)+1>=min_length]
+        names = [(first, last) for first in first_name for last in last_name if len(first)+len(last) + 1 >= min_length]
     full = []
     for i in names:
-        a=i[0][0].upper() + i[0][1:]
-        b=i[1][0].upper()+i[1][1:]
+        a = i[0][0].upper() + i[0][1:]
+        b = i[1][0].upper() + i[1][1:]
         full.append(" ".join([a,b]))
     return full
 
